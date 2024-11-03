a84b14b5172ff16fb80308d727d0d4ab2a166402
Mubashir1292 <mubashirliaqat72@gmail.com>
2024-11-03 19:14:45 +0500

    Typography Headings

---
+++ src/components/MUI_Typography.tsx
@@ -0,0 +1,18 @@
+import { Typography } from "@mui/material"
+export const MUITypography = () => {
+  return (
+    <div>
+        <Typography variant="h1">H1 Heading</Typography>
+        <Typography variant="h2">H2 Heading</Typography>
+        <Typography variant="h3">H3 Heading</Typography>
+        <Typography variant="h4" component="h1" gutterBottom>H4 Heading</Typography>
+        <Typography variant="h5">H5 Heading</Typography>
+        <Typography variant="h6">H6 Heading</Typography>
+        <Typography variant="subtitle1">Subtitle1</Typography>
+        <Typography variant="subtitle2">Subtitle2</Typography>
+        <Typography variant="body1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora tempore obcaecati, asperiores inventore explicabo voluptatum labore magnam aliquam porro blanditiis vero velit doloribus? Beatae voluptas numquam quae aspernatur laudantium. Esse!</Typography>
+        <Typography variant="body2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ex illum facere, voluptatum consequuntur ad blanditiis. Voluptatibus, molestias fuga. Provident a similique debitis, impedit distinctio quaerat veritatis magni adipisci, ullam nemo dolorem vitae corrupti voluptatum dicta ad sed eaque suscipit!</Typography>
+    </div>
+  )
+}
+
